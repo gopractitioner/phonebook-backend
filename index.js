19cca612baf1acb5f97de2d04ff403c8328d46a4
Junhui Zhang <junhuizhang.g@gmail.com>
2024-07-05 10:27:59 +1200

    c

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const password = process.argv[2]
 
 const url =
-    `mongodb+srv://chopsticksmemset:${password}@cluster0.dfttyvx.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://chopsticksmemset:mongodbroot123@cluster0.dfttyvx.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
