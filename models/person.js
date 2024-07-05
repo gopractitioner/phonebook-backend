392610827ee7a497a88738e8d7e8d6ea6e6b29d3
Junhui Zhang <junhuizhang.g@gmail.com>
2024-07-06 08:35:44 +1200

    extract mongo codes into its individual module

---
+++ models/person.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://chopsticksmemset:mongodbroot123@cluster0.dfttyvx.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+const personSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    number: String,
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Person = mongoose.model('Person', personSchema)
