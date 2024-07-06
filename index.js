39e3f3aa9172a3df83eb88580913b0ea549e6347
Junhui Zhang <junhuizhang.g@gmail.com>
2024-07-07 11:22:49 +1200

    error handling

--- index.js
+++ index.js
@@ -154,6 +154,20 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
