51f6391c9bcb5887f41887482ff75fd738851aeb
Junhui Zhang <junhuizhang.g@gmail.com>
2024-07-10 00:03:22 +1200

    test update

--- index.js
+++ index.js
@@ -154,13 +154,14 @@ app.delete('/api/persons/:id', (request, response, next) => {
     // response.status(204).end()
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
-            if (result) {
-                console.log('deleted...')
-                response.status(204).end()
-            } else {
-                console.log('not found...')
-                response.status(404).end()
-            }
+            // if (result) {
+            //     console.log('deleted...')
+            //     response.status(204).end()
+            // } else {
+            //     console.log('not found...')
+            //     response.status(404).end()
+            // }
+            response.status(204).end()
         })
         .catch(error => next(error))
 })
